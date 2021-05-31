3980716f20465f171f81f116ac3e487dcc893cc8
Griffin Flaxman <griffinflaxman27@gmail.com>
2021-05-31 11:19:16 -0400

    Add project files.

---
+++ Controllers/DatabaseAccess.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SilverlineSolutionsInterviewApp.Models;
+
+namespace SilverlineSolutionsInterviewApp.Controllers
+{
+    public class DatabaseAccess
+    {
+        public List<Recipe> GetAllRecipes()
+        {
+            using (var db = new RecipeList_SilverlineSolutionsEntities())
+                return db.Recipes.ToList();
+        }
+
+        public bool AddRecipe(string name, string image, string ingredients)
+        {
+            try
+            {
+                using (var db = new RecipeList_SilverlineSolutionsEntities())
+                {
+                    db.Recipes.Add(new Recipe() { 
+                        Name = name,
+                        ImageFile = image,
+                        Ingredients = ingredients
+                    });
+                    db.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool EditRecipe(int ID, string name, string ingredients, string image)
+        {
+            try
+            {
+                using (var db = new RecipeList_SilverlineSolutionsEntities())
+                {
+                    var recipe = db.Recipes.Find(ID);
+                    recipe.Name = name;
+                    if (!string.IsNullOrEmpty(image)) recipe.ImageFile = image;
+                    recipe.Ingredients = ingredients;
+                    db.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteRecipe(int ID)
+        {
+            try
+            {
+                using (var db = new RecipeList_SilverlineSolutionsEntities())
+                {
+                    db.Recipes.Remove(db.Recipes.Find(ID));
+                    db.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SilverlineSolutionsInterviewApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DatabaseAccess DatabaseAccess = new DatabaseAccess();
+        private readonly List<string> AllowedExtensions = new List<string> { "image/jpeg", "image/png", "image/tiff", "image/webp", "image/bmp" };
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(DatabaseAccess.GetAllRecipes());
+        }
+
+        [HttpPost]
+        public ActionResult AddNewRecipe(string Name, HttpPostedFileBase Image, string Ingredients)
+        {
+            if (AllowedExtensions.Contains(Image.ContentType))
+            {
+                //save the image to the folder
+                Image.SaveAs(Path.Combine(Server.MapPath("/Content/Images/"), Image.FileName));
+                //add the recipe in the database
+                if (DatabaseAccess.AddRecipe(Name, Image.FileName, Ingredients))
+                    return View("Index", DatabaseAccess.GetAllRecipes());
+            }
+            return Json("Failed to add the new recipe. Please try again.");
+        }
+
+        [HttpPost]
+        public ActionResult EditRecipe(int ID, string Name, string Ingredients, HttpPostedFileBase Image = null)
+        {
+            if (Image != null)//save the image to the folder
+                Image.SaveAs(Path.Combine(Server.MapPath("/Content/Images/"), Image.FileName));
+            if (DatabaseAccess.EditRecipe(ID, Name, Ingredients, Image != null ? Image.FileName : string.Empty))
+                return View("Index", DatabaseAccess.GetAllRecipes());
+            return Json("Failed to edit the recipe. Please try again.");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteRecipe(int ID)
+        {
+            if (DatabaseAccess.DeleteRecipe(ID))
+                return View("Index", DatabaseAccess.GetAllRecipes());
+            return Json("Failed to delete the recipe. Please try again.");
+        }
+    }
+}
